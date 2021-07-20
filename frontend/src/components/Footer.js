a15b8966beb6233abc6ee33c45d146c1a537c6a3
James <james.nyagatare@codeland.it>
2021-07-20 10:18:08 +0200

    Adds react-Bootstrap, header and footer components

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+const Footer = () => {
+    return (
+        <>
+           <footer>
+               <Container>
+                   <Row>
+                       <Col className='text-center py-3'>
+                         Copyright &copy; Eshop 
+                       </Col>
+                   </Row>
+               </Container>
+            </footer> 
+        </>
+    )
+}
+
+export default Footer
